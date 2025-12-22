c56b968d73627865a7815e26163c924a8e8ce94d
Ivan <128189489+IvanShehav@users.noreply.github.com>
2025-12-22 18:41:06 +0300

    Delete dop directory

--- dop/ЕГЭ/12/12.2.py
+++
@@ -1,9 +0,0 @@
-str = '5' * 874
-
-while '555' in str or '111' in str:
-    if '555' in str:
-        str = str.replace('555', '1', 1)
-    else:
-        str = str.replace('111','5',1)
-print(str.count('5'))
-print(str)
